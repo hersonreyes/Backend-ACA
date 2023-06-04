8850e03b37ff1965317af815406e28b8c6bba177
Alberto <alberto101481@gmail.com>
2023-06-03 20:58:12 -0600

    :sparkles: Add: validateFields middleware and validations for register

--- chat-backend/controllers/auth.js
+++ chat-backend/controllers/auth.js
@@ -1,25 +1,20 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const createUser = async(req, res = response) => {
 
+    const { name, email, password } = req.body;
+
     res.json({
         ok: true,
-        msg: 'New'
+        msg: 'New',
+        name,
+        email,
+        password
     });
 
 };
 
 const login = async(req, res = response) => {
-
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-           ok: false,
-           errors: errors.mapped() 
-        });
-    }
 
     const { email, password } = req.body;
 
--- chat-backend/routes/auth.js
+++ chat-backend/routes/auth.js
@@ -6,18 +6,29 @@ const { Router } = require('express');
 //Controllers
 const { createUser, login, renewToken } = require('../controllers/auth');
 const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 //Create new users
-router.post( '/new', createUser);
+router.post(
+    '/new',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password is required').not().isEmpty(),
+        validateFields
+    ],
+    createUser
+);
 
 //Login
 router.post(
     '/login',
     [
         check('email', 'Email is required').isEmail(),
-        check('password', 'Password is required').not().isEmpty()
+        check('password', 'Password is required').not().isEmpty(),
+        validateFields
     ], 
     login
 );
