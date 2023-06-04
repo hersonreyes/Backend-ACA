79470a2ed8a029dc6d72ca2e00357a6f4e80a610
Alberto <alberto101481@gmail.com>
2023-06-03 22:35:31 -0600

    :sparkles: Add: Create user feature

--- chat-backend/controllers/auth.js
+++ chat-backend/controllers/auth.js
@@ -1,16 +1,37 @@
 const { response } = require("express");
+const User = require('../models/user');
 
 const createUser = async(req, res = response) => {
 
-    const { name, email, password } = req.body;
+    try {
+        
+        const { name, email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'New',
-        name,
-        email,
-        password
-    });
+        //Verify Email 
+        const existEmail = await User.findOne({ email });
+        
+        if(existEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email already exists'
+            })
+        }
+
+        //Save User in DB
+        const user = new User(req.body);
+        await user.save();
+
+        res.json({
+            user
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact admin'
+        });
+    }
 
 };
 
